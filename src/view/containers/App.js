46d3fe0ae21224df17d8f78c462d29bf936dc555
shoponpon <shoponprog@gmail.com>
2017-10-16 15:25:12 +0900

    implement a flow on flux

--- src/view/containers/App.js
+++ src/view/containers/App.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -11,15 +5,22 @@ import {
   Text,
   View
 } from 'react-native';
+import UserStore from '../../store/UserStore';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
 
-export default class App extends Component<{}> {
+  static getStores(){
+    return [UserStore];
+  }
+
+  static calculateState(prevState){
+    return Object.assign(
+      {
+      },
+      UserStore.getState()
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -28,9 +29,6 @@ export default class App extends Component<{}> {
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
         </Text>
       </View>
     );
