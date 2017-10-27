d3dc638173656c3d5b3ac64f6240595a57ef03ed
shoponpon <shoponprog@gmail.com>
2017-10-28 04:04:45 +0900

    prevent warnnings

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
 import AppContainer from './src/view/containers/AppContainer';
 
+console.disableYellowBox = true;
+
 AppRegistry.registerComponent('ateam_internship_1month_client_app', () => AppContainer);
